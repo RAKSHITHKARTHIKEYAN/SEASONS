684c6537ee891fba233ec828116a3e2cda63144d
RAKSHITHKARTHIKEYAN <67536803+RAKSHITHKARTHIKEYAN@users.noreply.github.com>
2020-07-19 10:41:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function copy () {
+    alert('LOL!!!  Try not to be lazy.Type the content!!!');
+}
+function summer() {
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/dkd5jyhic/image/upload/v1594968327/Summer_tf9yzu.png')";
+    document.body.style.backgroundRepeat= "no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert("IT IS SUMMMER.");
+}
+function winter() {
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundRepeat= "no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert("IT IS WINTER.");
+}
+function spring() {
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundRepeat= "no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert("IT IS SPRING.");
+}
+
+function autumn() {
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundRepeat= "no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert("IT IS AUTUMN.");
+}
